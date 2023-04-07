0a3d212a995f93e92a08a385b44b339e30df475a
luanrem <luanrem@gmail.com>
2023-04-07 17:30:28 -0300

    P01C3 - Consumindo uma stream completa

--- 01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -14,8 +14,20 @@ class InverseNumberStrem extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverseNumberStrem()).pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent);
+
+  // return req.pipe(new InverseNumberStrem()).pipe(res);
 });
 
 server.listen(3334);
