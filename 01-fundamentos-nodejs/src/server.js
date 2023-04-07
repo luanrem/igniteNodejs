02525836302752908b33012cabf931c99c4cc8b0
luanrem <luanrem@gmail.com>
2023-04-07 16:36:50 -0300

    Estrutura da aplicação - Rotas de criação e listag

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -4,6 +4,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  console.log(method, url);
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuarios");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criacao de usuarios");
+  }
+
   return res.end("Hello World");
 });
 
