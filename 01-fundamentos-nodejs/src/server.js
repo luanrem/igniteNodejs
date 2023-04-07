73ffbc0dc4bd2c7e83f8611040ba2205e90affb1
luanrem <luanrem@gmail.com>
2023-04-07 17:39:02 -0300

    P01C3 - Corpo da requisicao em JSON

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -8,10 +8,23 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  console.log(method, url);
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  console.log(req.body);
+
   if (method === "GET" && url === "/users") {
     return res
       .setHeader("Content-type", "application/json")
@@ -19,10 +32,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
 
     return res.writeHead(201).end();
