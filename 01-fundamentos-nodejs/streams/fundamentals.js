bdeec8345ae0a355384b76e1715799985ca5f86b
luanrem <luanrem@gmail.com>
2023-04-07 16:59:38 -0300

    P01C3 - Criando stream de leitura

--- 01-fundamentos-nodejs/streams/fundamentals.js
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -1,3 +1,30 @@
 // Netflix & Spotify
 
 // Readable Streams / Writable Streams
+
+// Stream ->
+
+// Tudo que eu estou recebendo no stdin (terminal) estou enviando para a saida que eh stdout
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buff = Buffer.from(String(i));
+
+        this.push(buff);
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
