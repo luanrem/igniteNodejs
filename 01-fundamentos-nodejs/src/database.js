3d68f3d4fcd871f603412504133b69296ee1a5de
luanrem <luanrem@gmail.com>
2023-04-10 21:09:52 -0300

    P01C4 - Persistindo banco de dados

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -1,7 +1,28 @@
 // {"users": ...}
+import fs from "node:fs/promises";
+
+// console.log(import.meta.url);
+
+const databasePath = new URL("../db.json", import.meta.url);
+
+// console.log(databasePath.pathname);
 
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
 
   select(table) {
     const data = this.#database[table] ?? [];
@@ -15,6 +36,8 @@ export class Database {
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
