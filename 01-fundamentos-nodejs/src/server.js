e8d370864f6c6abd98a5ce9b151484c3654cf6c9
luanrem <luanrem@gmail.com>
2023-04-10 21:34:10 -0300

    P01C5 - Separando rotas da aplicação

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,41 +2,23 @@
 // ESModules ==> import
 // internal module from node you need to import like "node:<package>"
 import http from "node:http";
-import { randomUUID } from "node:crypto";
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
 
 // JSON - Javascript Object Notation
 // Cabeçalhos (Requisição;resposta) => Metadados
 
-const database = new Database();
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  // middlewares sempre passam como parametro req e res
   await json(req, res);
 
-  console.log(req.body);
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
-
-    return res.end(JSON.stringify(users));
-  }
-
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.writeHead(201).end();
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
