5ef5fe2665ce6282fcd7e464e299264c46a46d14
luanrem <luanrem@gmail.com>
2023-04-10 22:02:09 -0300

    P01C5 - Rotas com parâmetros (RegEx)

--- 01-fundamentos-nodejs/src/routes.js
+++ 01-fundamentos-nodejs/src/routes.js
@@ -33,7 +33,7 @@ export const routes = [
   },
   {
     method: "DELETE",
-    path: buildRoutePath("/users/:id"),
+    path: buildRoutePath("/users/:id/groups/:groupid"),
     handler: (req, res) => {
       return res.end();
     },
