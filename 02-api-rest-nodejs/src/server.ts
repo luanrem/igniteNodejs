90a8e1de29b9ccd0f1e0e6bd9bf2ed1653a555e3
luanrem <luanrem@gmail.com>
2023-04-16 19:06:56 -0300

    P02C1 - Criando aplicação

---
+++ 02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+// GET, POST, PU, PATCH, DELETE
+
+app.get("/hello", () => {
+  return "Hello World";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running!");
+  });
