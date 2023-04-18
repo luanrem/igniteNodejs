38375bfa9d62b528727e4b1c494468a72369686c
luanrem <luanrem@gmail.com>
2023-04-18 19:06:18 -0300

    P02C3 - Plugins do Fastify

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,27 +1,11 @@
 import fastify from 'fastify'
 // import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// GET, POST, PU, PATCH, DELETE
-
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
