6fd320023c1cade3e1b280c8c35bc5a5273f4877
luanrem <luanrem@gmail.com>
2023-04-30 10:39:43 -0300

    P02C4 - Criando primeiro teste

---
+++ 02-api-rest-nodejs/test/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+  // fazer a chamada http
+  // validacao
+
+  const responseStatusCode = 500
+
+  expect(responseStatusCode).toEqual(201)
+})
