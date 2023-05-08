d5b5ea051ae5125d48ef41f4d438f28b3528de2c
luanrem <luanrem@gmail.com>
2023-05-07 21:24:34 -0300

    P03C3 - Inversão de dependências

--- 03-api-solid/src/use-cases/register.ts
+++ 03-api-solid/src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,28 +7,37 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+// SOLID
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+// D => Dependency Inversion Principle
+
+export class RegisterUseCase {
+  // private usersRepository: any
+  // constructor(usersRepository: any) {
+  //   this.usersRepository = usersRepository
+  // }
+
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository()
+
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
