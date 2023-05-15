bb6efeb8cd2d001a165a076ffb01b1b267cb3f44
luanrem <luanrem@gmail.com>
2023-05-15 20:54:53 -0300

    P03C6 - Controllers das rotas de check-ins

---
+++ 03-api-solid/src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const checkIn = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page } = checkIn.parse(request.query)
+
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+    userId: request.user.sub,
+    page,
+  })
+
+  return reply.status(201).send({
+    checkIns,
+  })
+}
