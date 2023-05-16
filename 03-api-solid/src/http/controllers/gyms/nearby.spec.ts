ca2aa522695147df61bf9331af722d203a1936c6
luanrem <luanrem@gmail.com>
2023-05-15 21:46:42 -0300

    P03C6 - Testes E2E de rotas de check-ins

--- 03-api-solid/src/http/controllers/gyms/nearby.spec.ts
+++ 03-api-solid/src/http/controllers/gyms/nearby.spec.ts
@@ -46,8 +46,6 @@ describe('Nearby Gym (e2e)', () => {
       .set('Authorization', `Bearer ${token}`)
       .send()
 
-    console.log(response.body)
-
     expect(response.statusCode).toEqual(200)
     expect(response.body.gyms).toHaveLength(1)
     expect(response.body.gyms).toEqual([
